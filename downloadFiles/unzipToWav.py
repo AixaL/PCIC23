1015c4dce42d30f340dd2ab36e106fc430c51203
Aixa <aixa.lievana@gmail.com>
2023-07-03 20:56:01 -0600

    test

--- downloadFiles/unzipToWav.py
+++ downloadFiles/unzipToWav.py
@@ -49,9 +49,10 @@ for file in audio_files:
     print(file)
     carpeta = file_name + '_audios'
     if file_name != 'CCv2_part_10' or file_name != 'CCv2_annotations':
+        print('entro')
     # loading the temp.zip and creating a zip object
-        with ZipFile(file, 'r') as zObject:
-            zObject.extractall(path=carpeta)
-            convert_to_wav(carpeta+'/')
+        # with ZipFile(file, 'r') as zObject:
+        #     zObject.extractall(path=carpeta)
+        #     convert_to_wav(carpeta+'/')
     else:
         print('Carpeta No')
