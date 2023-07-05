5959f823a264634ce474d71484072eba6afa981f
Aixa <aixa.lievana@gmail.com>
2023-07-04 21:16:59 -0600

    create get links

---
+++ downloadFiles/getLinks.js
@@ -0,0 +1,10 @@
+a_tabs.forEach((aTag) => {
+  // Get the href and innerText
+  const href = aTag.href;
+  const innerText = aTag.innerText;
+  
+  // Do something with the href and innerText
+  console.log(innerText , href);
+});
+
+let a_tabs = document.querySelectorAll('.x1sfzahb.x1odaek6')
