b103016a3f050c9a3fc8974919f74ef3c40e8f37
Aixa <aixa.lievana@gmail.com>
2023-07-06 13:53:37 -0600

    errorAudio

---
+++ downloadFiles/pruebaCarpetaError.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+import sys
+import glob
+import pandas as pd
+# importing the zipfile module
+from zipfile import ZipFile
+from moviepy.editor import *
+
+current_directory = os.getcwd()
+print("Directorio actual:", current_directory)
+
+
+def runcmd(cmd, verbose = False, *args, **kwargs):
+
+    process = subprocess.Popen(
+        cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True,
+        shell = True
+    )
+    std_out, std_err = process.communicate()
+    if verbose:
+        print(std_out.strip(), std_err)
+    pass
+
+def convert_to_wav(audio_dir):
+    print(audio_dir)
+    contenido = os.listdir(audio_dir)
+    videos = []
+    for fichero in contenido:
+        if os.path.isfile(os.path.join(audio_dir, fichero)) and fichero.endswith('.mp4'):
+            print(fichero)
+            videos.append(fichero)
+
+    for video in videos:
+        print(audio_dir)
+        audioclip = AudioFileClip(audio_dir + video)
+        videoWav= video.replace('mp4' , 'wav')
+        audioclip.write_audiofile(audio_dir + videoWav) 
+        runcmd('rm '+ audio_dir + '/' +video , verbose = True)
+
+carpeta = 'CCv2_part_48_audios'
+convert_to_wav(carpeta+'/')
