ec00953bdf6843a4f231a0e11383e310109c6407
Aixa <aixa.lievana@gmail.com>
2023-10-26 21:09:59 -0600

    nuevos links

--- downloadFiles/download_links.py
+++ downloadFiles/download_links.py
@@ -22,14 +22,14 @@ def runcmd(cmd, verbose = False, *args, **kwargs):
     pass
 
 
-ruta_archivo_links = "Links190623.txt"
-datos_link = pd.read_csv(ruta_archivo_links, sep=" ",header=None,names=["name", "link"])
+ruta_archivo_links = "First20.csv"
+datos_link = pd.read_csv(ruta_archivo_links,names=['file_name','cdn_link','number'])
 
 print(datos_link.head())
 
 for index, row in datos_link.iterrows():
-    file_name = row['name']
-    cdn_link = row['link']
+    file_name = row['file_name']
+    cdn_link = row['cdn_link']
     link_wget= 'wget -O '+ file_name +' "'+ cdn_link+'"'
     # link_wget= 'sudo wget "'+ cdn_link+'"'
     print(''+link_wget+'')
